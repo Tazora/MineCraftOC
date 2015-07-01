22584935bc097a52a6f6c6be102777b4db0ae9dc
Tazora <sebastian-thon@web.de>
2015-07-01 20:42:59 +0200

    16x16x5 Loch

---
+++ vorwaerts.lua
@@ -0,0 +1,37 @@
+local robot = require("robot")
+local function dig()
+  while robot.detect()==true do
+    robot.swing()
+  end
+end
+local function go()
+  for i=0,15,1 do
+    dig()
+    robot.forward()
+  end
+end
+local function ebene()
+for i=0,8,1 do
+go()
+robot.turnRight()
+dig()
+robot.forward()
+robot.turnRight()
+go()
+robot.turnLeft()
+dig()
+robot.forward()
+robot.turnLeft()
+end
+robot.turnLeft()
+go()
+robot.turnRight()
+end
+
+for i=0,4,1 do
+while robot.detect()==true do
+robot.swingDown()
+end
+robot.down()
+ebene()
+end
